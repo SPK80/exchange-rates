00aa0ca4238ad594bb9cb27ef6aed60b9089f973
SPK80 <psukonkin@gmail.com>
2022-03-22 06:54:03 +0300

    feat: add Table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
+import { Table } from 'antd';
 import './App.css';
+import 'antd/dist/antd.css';
+
+import daily from './daily.json';
 
 function App() {
+
+
+	const dataSource = [
+		...Object.values(daily.Valute).map(v => {
+
+			return { key: v.ID, name: v.Name, value: v.Value }
+		})
+	];
+	console.log(dataSource);
+
+	// function onChange(pagination, filters, sorter, extra) {
+	// 	console.log('params', pagination, filters, sorter, extra);
+	// }
+	// const dataSource = [
+	// 	{
+	// 		key: '1',
+	// 		name: 'Mike',
+	// 		age: 32,
+	// 		address: '10 Downing Street',
+	// 	},
+	// 	{
+	// 		key: '2',
+	// 		name: 'John',
+	// 		age: 42,
+	// 		address: '10 Downing Street',
+	// 	},
+	// ];
+	// console.log(dataSource);
+	const columns = [
+		{
+			title: 'Name',
+			dataIndex: 'name',
+			key: 'key',
+		},
+		{
+			title: 'Value',
+			dataIndex: 'value',
+			key: 'key',
+		},
+
+	];
+
 	return (
-		<div>
-		</div>
+		<Table
+			size={'middle'}
+			dataSource={dataSource}
+			columns={columns}
+		/>
 	);
 }
 
