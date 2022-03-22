19dfc9a8fb4f7ae5e42450a7a86fd671ad527397
SPK80 <psukonkin@gmail.com>
2022-03-22 16:57:41 +0300

    refactor: Tooltip

--- src/App.js
+++ src/App.js
@@ -1,49 +1,78 @@
-import { Table } from 'antd';
+import { Table, Tooltip } from 'antd';
 import './App.css';
 import 'antd/dist/antd.css';
 import daily from './daily.json';
 
 function App() {
 
-
 	const dataSource = [
-		...Object.values(daily.Valute).map(v => {
-
-			return { key: v.ID, name: v.Name, value: v.Value }
-		})
+		...Object.values(daily.Valute)
+			.map(v => ({
+				...v,
+				key: v.ID,
+				delta: (`(${(v.Value - v.Previous).toFixed(1)})`)
+			}
+			))
 	];
-	console.log(dataSource);
 
 	const columns = [
-		{
-			title: 'Валюта',
-			dataIndex: 'name',
-			key: 'key',
-			// align: 'left',
-			className: 'Name',
-			width: '80%',
-		},
-		{
-			title: 'Value',
-			dataIndex: 'value',
-			key: 'key',
-			// align: 'right',
-			width: '20%',
 
+		// {
+		// 	title: 'Валюта',
+		// 	dataIndex: 'Name',
+		// 	key: 'ID',
+		// 	width: '40%',
+		// },
+		{
+			title: 'Букв. код',
+			dataIndex: 'CharCode',
+			key: 'key',
 		},
+		{
+			title: 'Курс',
+			colSpan: 2,
+			dataIndex: 'Value',
+			key: 'key',
+		},
+		{
+			title: 'Изменение курса',
+			colSpan: 0,
+			dataIndex: 'delta',
+			key: 'key',
+			width: '10%',
+		},
+		// {
+		// 	title: 'Цифр. код',
+		// 	dataIndex: 'NumCode',
+		// 	key: 'ID',
+		// },
+
+		// {
+		// 	title: 'Единиц',
+		// 	dataIndex: 'Nominal',
+		// 	key: 'ID',
+		// },
 
 	];
 
 	return (
 
 		<div className='Wrapper'>
-
-			<Table
-				size={'middle'}
-				dataSource={dataSource}
-				columns={columns}
-			/>
-
+			<div className='Table'>
+				<Table
+					size={'middle'}
+					dataSource={dataSource}
+					columns={columns}
+					pagination={{ pageSize: 20 }}
+					onRow={(record, index) => ({
+						// onClick: event => { console.log(event) }, // click row
+						// onDoubleClick: event => { console.log(event) }, // double click row
+						// onContextMenu: event => { console.log(event) }, // right button click row
+						onMouseEnter: event => { console.log(event) }, // mouse enter row
+						onMouseLeave: event => { console.log(event) }, // mouse leave row
+					})}
+				/>
+			</div>
 		</div>
 	);
 }
